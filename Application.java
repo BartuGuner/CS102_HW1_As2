01b78cf8b351ebe9b1362768b1ff86ba18cbad70
Ahmet <merteroglu2004@gmail.com>
2025-02-06 19:30:06 +0300

    test

--- Application.java
+++ Application.java
@@ -1,5 +1,5 @@
 public class Application{
-    public int list[] = new int[100];//hjgjhgjhgjhgdaahs
+    public int list[] = new int[100];//hjgjhgjhgjhgdaahsdsfsdf
     public static void main(String[] args) 
     {
         
